06828761120fcffa226755fb4e2734f63e0e2ace
ladykate17 <stamm.katherine@gmail.com>
2015-07-03 15:01:49 -0600

    completed exercise

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ $(document).on('ready', function() {
 // 1.) Write a function called 'getName' which takes a single object argument and returns the value of the 'name' property of the given object.
 
 	var getName = function(object){
-		return console.log( object.name );
+		console.log( object.name );
+		return object.name;
 	}
 
 	getName({ name: 'Luisa', age: 25 }); // should return 'Luisa'
@@ -19,7 +20,8 @@ $(document).on('ready', function() {
 		var b = array.join()
 		var c = (b.length) - a // subtracts the comma count from the joined array
 
-		return console.log( c );
+		console.log( c );
+		return c;
 	}
 
 	totalLetters(['javascript', 'is', 'awesome']) //should return 19
@@ -36,7 +38,8 @@ $(document).on('ready', function() {
 		var a =  Object.defineProperty(obj, key, { value: val})
 		
 
-		return console.log( a );
+		console.log( a );
+		return a;
 		
 	};
 
@@ -49,7 +52,8 @@ $(document).on('ready', function() {
 	var negativeIndex = function(array, negIndex){
 
 		var a = array.length - (negIndex * -1)
-		return console.log( array[a] );
+		console.log( array[a] );
+		return array[a];
 	}
 
 
@@ -64,12 +68,13 @@ $(document).on('ready', function() {
 	var removeM = function(string){
 
 		var a = string.replace('m', '')
-		return console.log(a);
+		return a;
+		console.log(a);
 	}
 
 	removeM('family') // should return 'faily'
 	removeM('memory') // should return 'eory'
-
+	
 
 
 
@@ -104,61 +109,114 @@ $(document).on('ready', function() {
 
 // 7.) Write a function called 'vowels' which takes a string and returns an array of all the vowels in the string, including duplicates.
 
+
+		var vowels = function(str){
+
+			var validVowels = 'aeiouy';
+			var output = [];
+
+		// check character at each index of passed string against each element in array
+			for(var i = 0; i < str.length; i++){
+				// if true --> push that character to new array
+				if(validVowels.indexOf(str[i]) > -1) { // iterate over validValues array and check next to  each charater in atring // str[] --> str.charAt(i)
+					output.push(str[i]);
+				};
+			
+			};
+
+			// return completed array
+			console.log( output )
+			return output;
+
+		};
+
+
 	
-	var vowels = function(string){
-		
-		var validVowels = ['a', 'e', 'i', 'o', 'u', 'y']
-
-		// var checkInstance = function(a){
-		// 	if (string.toLowerCase().indexOf(0) === -1 ) {
-		// 		return []
-		// 	}
-		// }
-
-		return console.log(string.toLowerCase());
-
-
-		//SAMPLE CODE
-		// var str = '12345';
-		// [].map.call(str, function(x) {return x;}).reverse().join(''); 
-
-	}
-
-
-	
-	vowels('alabama') // should return ['a', 'a', 'a', 'a']
-	vowels('What evil odd ducks!') // should return ['a', 'e', 'i', 'o', 'u']
+	vowels('alabama'); // should return ['a', 'a', 'a', 'a']
+	vowels('What evil odd ducks!'); // should return ['a', 'e', 'i', 'o', 'u']
 
 
 
 
 // 8.) Write a function called 'twins' which takes an array and returns true if every adjacent pair of items in the array is the same.
 
-	// var twins =
-	// twins(['a', 'a', 'b', 'b', 'c', 'c']) // should return true
-	// twins(['a', 'a', 'b', 'c', 'd', 'd']) // should return false
-	// twins(['a', 'a', 'b', 'z']) // should return false
-	// twins(['a', 'a', undefined]) // should return false
+	var twins = function(array){
+
+		var allTwins = true;
+
+		for(var i = 0; i < array.length; i+=2) {
+			
+			var a = array.slice(i, (i+2));
+
+			if ( a[0] !== a[1] || a[0] === undefined ) {
+
+				allTwins = false;
+			};
+
+		};
+		
+		console.log( allTwins )
+		return allTwins;
+
+	};
+
+
+
+	twins(['a', 'a', 'b', 'b', 'c', 'c']) // should return true
+	twins(['a', 'a', 'b', 'c', 'd', 'd']) // should return false
+	twins(['a', 'a', 'b', 'z']) // should return false
+	twins(['a', 'a', undefined]) // should return false
 
 
 
 
 // 9.) Write a function called 'or' which takes an array of booleans and returns true if any one of them is true. Given an empty array, return false. If you find an item that is true, the function should return true immediately and not continue checking further values.
 
+	var or = function(array){
 
-	// or([false, false, true, false]) // should return true
-	// or([false, false, false]) // should return false
-	// or([]) // should return false
+		// console.log(array.indexOf(true));
+
+		if ( (array.indexOf(true)) > -1 ) {
+			console.log( true );
+			return true;
+		}
+
+		else {
+			console.log( false );
+			return false;
+		}
+		
+	}
+
+	or([false, false, true, false]) // should return true
+	or([false, false, false]) // should return false
+	or([]) // should return false
 
 
 
 
 // 10.) Write a function called 'unique' which takes an array of strings, and returns a new array consisting of the unique values (no duplicates).
 	
-	
 
-	// unique(['a', 'b', 'a', 'c', 'd', 'd']) // should return ['a', 'b', 'c', 'd']
-	// unique(['todd', 'avery', 'maria', 'avery']) // should return ['todd', 'avery', 'maria']
+	var unique = function(array){
+		
+	var uniqueValues = []
+		
+		for (var i = 0; i < array.length; i++){
+			// console.log(i, array[i])
+			if ( uniqueValues.indexOf(array[i]) === -1 ) {
+				uniqueValues.push(array[i])
+			};
+
+		};
+		console.log(uniqueValues);
+		return uniqueValues;
+
+	};
+
+
+	unique(['a', 'b', 'a', 'c', 'd', 'd']) // should return ['a', 'b', 'c', 'd']
+	unique(['todd', 'avery', 'maria', 'avery']) // should return ['todd', 'avery', 'maria']
 
 
 
